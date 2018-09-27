31589ed2ae890e41cbbd4b5183ad5bbcfe6de5d8
jwhlaforest <jwh.laforest@gmail.com>
2018-09-26 21:37:18 -0500

    part 1 complete, looking into part 2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,17 +7,6 @@ console.log(tableData);
 
 var tbody = d3.select("tbody");
 
-// data.forEach(function(UFOsighthings) {
-//   console.log(UFOsighthings);
-//   var row = tbody.append("tr");
-
-//   Object.entries(UFOsighthings).forEach(function([key, value]) {
-//     console.log(key, value);
-//     var cell = tbody.append("td");
-//     cell.text(value);
-//   });
-// });
-
 data.forEach((UFOsighthings) => {
   console.log(UFOsighthings);
   var row = tbody.append("tr");
@@ -28,3 +17,31 @@ data.forEach((UFOsighthings) => {
   });
 });
 
+var submit = d3.select("#submit");
+
+submit.on("click", function() {
+  d3.event.preventDefault();
+
+  d3.select(".summary").html("");
+
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  filteredData.forEach((dateData) => {
+    console.log(dateData);
+    var row = tbody.append("tr");
+    Object.entries(dateData).forEach(([key, value]) => {
+      console.log(key, value);
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+});
